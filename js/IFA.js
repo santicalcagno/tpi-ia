ed4c18bef620346f629745f07461789d6a383b8d
julian.rd7 <julian.rd7@gmail.com>
2016-07-02 12:40:42 -0300

    IFA probado para COMET SATURN URANUS 3/10

--- js/IFA.js
+++ js/IFA.js
@@ -97,15 +97,22 @@ function getDistance(ff1, ff2, letters){
 	return Math.sqrt(rij);
 }
 
-function attract(ff1, ff2, b, alfa, letters){
+function attract(ff1, ff2, betaMin, beta0, alfa, gamma, letters){
 	// Realiza el movimiento de la luciérnaga 'ff1' hacia 'ff2' mediante la función de acercamiento y la
 	// utilización de la función closest
+
+	// Cálculo de la distancia
+	var rij = getDistance(ff1, ff2, letters);
+
+	// Actualización del valor beta
+	var beta = betaMin + (beta0 - betaMin)*Math.exp(-gamma*Math.pow(rij,2));
+
 	var availableNums = [0,1,2,3,4,5,6,7,8,9];
 	shuffle(availableNums);
 	letters = letters.reverse();
 	for (var i in letters){
 		var k =  letters[i];
-		var newNum = Math.round(ff1[k] + b*(ff2[k] - ff1[k]) + alfa*(Math.random()-0.5));
+		var newNum = Math.round(ff1[k] + beta*(ff2[k] - ff1[k]) + alfa*(Math.random()-0.5));
 		if (!(has(newNum,availableNums))){
 			newNum = closest(newNum, availableNums);
 		}
@@ -116,7 +123,7 @@ function attract(ff1, ff2, b, alfa, letters){
 	return ff1;
 }
 
-function getLetters(words) {
+function getLetters(words, operation) {
 	// Devuelve un arreglo con las distintas letras que posee el problema en orden de aparición
 	var letters = [];
 	var revWords = [];
@@ -125,7 +132,9 @@ function getLetters(words) {
 		revWords.push(reverse(w));
 	}
 	var j = 0;
-	while (j < revWords[(revWords.length) - 1].length) {
+	if (operation == "suma") var maxLenght =  revWords[(revWords.length) - 1].length;
+	else var maxLenght =  revWords[0].length;
+	while (j < maxLenght) {
 		for (i in revWords){
 			w = revWords[i];
 			if (j < w.length && !(w[j] in letters)) {
@@ -152,7 +161,7 @@ function generateFireflies(ni, letters){
 	return fireFlies;
 }
 
-function  findFittest(fireFlies, words, operation) {
+function  findTheFittest(fireFlies, words, operation) {
 	// Devuelve la luciérnaga más apta
 	var mostFit = fireFlies[0];
 	for (var i in fireFlies){
@@ -164,8 +173,19 @@ function  findFittest(fireFlies, words, operation) {
 	return mostFit;
 }
 
+function findFittestGroup(fittest, fireFlies, words, operation){
+	var fittestGroup = [];
+	for (var f in fireFlies){
+		var ff = fireFlies[f];
+		if (Math.abs(getIntensity(ff, words, operation)) == fittest)
+			fittestGroup.push(ff);
+	}
+}
+
 function findTop(fireFlies, words, operation, topNum, topFF){
 	// Devuelve las 'topNum' luciérnagas mas aptas
+
+	// Caso base
 	if (topNum == 0){
 		for (var f in topFF){
 			var ff = topFF[f];
@@ -173,8 +193,10 @@ function findTop(fireFlies, words, operation, topNum, topFF){
 		}
 		return topFF;
 	}
+
+	// Caso recursivo
 	else {
-		var fittest = findFittest(fireFlies, words, operation);
+		var fittest = findTheFittest(fireFlies, words, operation);
 		topFF.push(fittest);
 		var index = fireFlies.indexOf(fittest);
 		fireFlies.splice(index,1);
@@ -182,34 +204,43 @@ function findTop(fireFlies, words, operation, topNum, topFF){
 	}
 }
 
-function mutatePopulation(fireFlies, previousTop, words, operation, alfaMax, letters){
+function mutatePopulation(fireFlies, alfa, betaMin, beta0, gamma, letters){
 	// Realiza una mutación a la población entera
-	for (var i in fireFlies){
-		var ff = fireFlies[i];
-		ff = attract(ff, ff, 0, alfaMax, letters);
+	var j = 0;
+	while (j < 3){ 
+		for (var i in fireFlies){
+			var ff = fireFlies[i];
+			ff = attract(ff, generateFirefly(letters), betaMin, beta0, alfa, gamma, letters);
+		}
+		j = j + 1;
 	}
 	return fireFlies;
 }
 
-function fireflies(words, operation){
+function fireflies(words, operation, ni, topNum, alfa, alfaMax, gamma, beta0, betaMin, maxSameTop, maxGen){
+//function fireflies(words, operation){
 	// Algoritmo de luciérnagas inteligente
 
 	// Inicialización de variables
-	var gamma = 1;
-	var alfa = 0.8;
-	var ni = 15;
-	var topNum = Math.round(ni/5);
-	var alfaMax = 8;
-	var betaMin = 0.2;
-	var beta0 = 1;
-	var beta = beta0;
-	var letters =  getLetters(words);
+
+	/* var gamma = 1;
+	 var alfa = 0.7;
+	 var ni = 20;
+	 var topNum = Math.round(ni/5);
+	 var alfaMax = 8;
+	 var betaMin = 0.2;
+	 var beta0 = 1;
+	 var beta = beta0;
+	 var maxSameTop = 10;
+	 var maxGen = 1500;
+	*/
+	var letters =  getLetters(words, operation);
 	var fireFlies = generateFireflies(ni, letters);
 	var i = 0;
 	var iterations = 0;
 	var previousTop = 0;
 	var sameTop = 0;
-	var maxGen = 10000;
+
 
 	//Inicio
 	while (iterations < maxGen){
@@ -218,9 +249,11 @@ function fireflies(words, operation){
 		// Comprobación de brillo de las luciérnagas.
 		for (var k in fireFlies){
 			var ff = fireFlies[k];
+			//console.log(getIntensity(ff, words, operation));
 			if (getIntensity(ff, words, operation) == 0){
 				//Solución encontrada
-				return ff;
+				//return ff;
+				return iterations;
 			}
 		}
 
@@ -228,14 +261,14 @@ function fireflies(words, operation){
 		var topFF = findTop(fireFlies, words, operation, topNum, []);
 
 		// Comprobar que la intensidad de la mejor haya variado desde el último ciclo
-		if (Math.abs(getIntensity(findFittest(topFF, words, operation), words, operation)) == Math.abs(previousTop)) sameTop = sameTop + 1;
+		if (Math.abs(getIntensity(findTheFittest(topFF, words, operation), words, operation)) == Math.abs(previousTop)) sameTop = sameTop + 1;
 		else {
 			previousTop = getIntensity(topFF[0], words, operation);
 			sameTop = 0;
 		}
 
 		// Si no ha variado en 5 ciclos, se muta la población
-		if (sameTop > 5) fireFlies = mutatePopulation(fireFlies, previousTop, words, operation, alfaMax, letters);
+		if (sameTop > maxSameTop) fireFlies = mutatePopulation(fireFlies, alfa, betaMin, beta0, gamma, letters);
 
 		// Comparación de luciérnagas
 		i = 0;
@@ -245,18 +278,53 @@ function fireflies(words, operation){
 				var Ii = getIntensity(fireFlies[i], words, operation);
 				var Ij = getIntensity(ffj, words, operation);
 				if (Math.abs(Ij) < Math.abs(Ii)){
-					// Cálculo de la distancia
-					var rij = getDistance(fireFlies[i], ffj, letters);
-					// Actualización del valor beta
-					beta = betaMin + (beta0 - betaMin)*Math.exp(-gamma*Math.pow(rij,2));
 
 					// Movimiento de ffi hacia ffj
-					fireFlies[i] = attract(fireFlies[i], ffj, beta, alfa, letters);
+					fireFlies[i] = attract(fireFlies[i], ffj, betaMin, beta0, alfa, gamma, letters);
 				}
 			}
 			i = i + 1;
 		}
+
+		// Movimiento aleatorio de las luciernagas de mejor intensidad que no se movieron en esta iteracion
+		notAttracted = findFittestGroup(getIntensity(findTheFittest(fireFlies, words, operation), words, operation), fireFlies, words, operation)
+		for (k in notAttracted){
+			ff = notAttracted[k];
+
+			// Movimiento aleatorio de ff
+			ff = attract(ff, ff, 0, 0, alfaMax, gamma, letters);
+		}
 	}
 	// Si no se encuentra solucion luego de 'maxGen' ciclos, devuelve nulo
-	return null;
+	//return null;
+	return iterations;
 }
+
+maxGen = 1500;
+var MAX_TESTS = 10;
+console.log("Entré");
+var testCases = [];
+for (var i = 0; i < MAX_TESTS; i++) {
+	testCases[i] = fireflies(["COMET", "SATURN", "URANUS"], "suma", 50, 5, 0.9, 8, 1, 0, 0.5, 3, maxGen);
+	if (testCases[i] < maxGen)
+		console.log("Resultado " + (i+1) + " hallado");
+	else
+		console.log("Resultado " + (i+1) + " no hallado en " + maxGen + " iteraciones")
+}
+console.log("Salí");
+
+console.log("Cantidad de fireflies: 10");
+
+var testAvg = 0;
+var maxIter = 0;
+var minIter = 9999;
+for (var i = 0; i < testCases.length; i++) {
+	testAvg += testCases[i];
+	if (testCases[i] > maxIter) maxIter = testCases[i];
+	if (testCases[i] < minIter) minIter = testCases[i];
+}
+testAvg /= testCases.length;
+
+console.log("Promedio: " + testAvg);
+console.log("Maxima iteracion: " + maxIter);
+console.log("Minima iteracion: " + minIter);
